8b9636cbcd35485f3ace02f9044a978dc12b6aa5
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-02-19 20:18:00 -0500

    deploy: gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// solo si tambien vamos a implementar el deplieuge desde github actions
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+
+// Configuración base dependiendo de si se ejecuta localmente o en GitHub Actions
+const base = isGitHubActions ? '/deploy_vitest-gh-pages' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
